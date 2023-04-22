87bf5a703b3e8a1b07e1a3e8bc35c467de0cde60
vd89 <dixitvora@gmail.com>
2023-04-22 15:26:52 +0530

    #1-setup: updated the code for the app file and eslint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,43 @@
 import express from 'express';
+import appConfig from './appConfig.js';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+const { whiteList } = appConfig;
+
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whiteList.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = {
+      origin: true,
+      credentials: true,
+      optionsSuccessStatus: 200,
+      methods: ['OPTIONS', 'HEAD', 'GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    };
+  } else {
+    corsOptions = {
+      origin: false,
+      credentials: true,
+      optionsSuccessStatus: 200,
+      methods: ['OPTIONS', 'HEAD', 'GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    };
+  }
+  callback(null, corsOptions);
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+app.use(helmet());
+app.use('*', cors(corsOptionsDelegate));
+
+
+
+
+
 
 export default app;
